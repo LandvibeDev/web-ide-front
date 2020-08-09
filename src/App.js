b217a54d2ca3263c51ed2898186ea72eb03657b9
kang-jisu <rkdwltn0425@gmail.com>
2020-08-09 16:45:20 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import SignInView from './signin/view/SignInView';
+import SignUpView from './signin/view/SignUpView';
+import WorkSpaceMain from './workspace/WorkSpaceMain';
+import DashBoardMain from './dashboard/DashBoardMain';
 
-import { WorkSpaceMain } from './workspace';
 function App() {
   return (
     <div className="App">
-      <WorkSpaceMain />
+      <BrowserRouter>
+        <Route path="/signin" component={SignInView} />
+        <Route path="/signup" component={SignUpView} />
+        <Route path="/workspace" component={WorkSpaceMain} />
+        <Route path="/dashboard" component={DashBoardMain} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/dashboard/DashBoardMain.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DashBoardMain(props) {
+  return <div></div>;
+}
+
+export default DashBoardMain;
