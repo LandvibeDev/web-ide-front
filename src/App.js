85bfa579ed0deb6782fa6765bd83ee030aa81afa
JIWON KWAK <koagjw@gmail.com>
2020-08-09 18:32:41 +0900

    signin/view -> signin/container 폴더명 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import SignInView from './signin/view/SignInView';
-import SignUpView from './signin/view/SignUpView';
+import SignInView from './signin/container/SignInView';
+import SignUpView from './signin/container/SignUpView';
 import { WorkSpaceMain } from './workspace';
 import { DashBoardMain } from './dashboard';
 
