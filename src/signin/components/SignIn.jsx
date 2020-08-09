bd7898602b81c16c1696887f4565b2eab773dd22
gjm1026 <gjm1026@naver.com>
2020-08-09 22:42:04 +0900

    Revert "conflict resolve"
    This reverts commit 7e794c746699d61581f5b85483dea3adad64a73e.

--- src/signin/components/SignIn.jsx
+++ src/signin/components/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import GitHubLogin from 'react-github-login';
 import { Grid } from '@material-ui/core';
