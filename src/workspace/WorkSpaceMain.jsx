6e963d675982f48ea0f6855379c5bb3f51bcb266
kang-jisu <rkdwltn0425@gmail.com>
2020-08-11 00:47:25 +0900

    뒤로가기 방지

--- src/workspace/WorkSpaceMain.jsx
+++ src/workspace/WorkSpaceMain.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Header, Body } from './components';
 const useStyles = makeStyles((theme) => ({
@@ -10,6 +10,17 @@ const useStyles = makeStyles((theme) => ({
 
 function WorkSpaceMain(props) {
   const classes = useStyles();
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', function (event) {
+      event.returnValue = 'workspace에서 나가시겠습니까?';
+    });
+    return () => {
+      window.removeEventListener('beforeunload', function (event) {
+        event.returnValue = 'workspace에서 나가시겠습니까?';
+      });
+    };
+  }, []);
 
   return (
     <div className={classes.root}>
