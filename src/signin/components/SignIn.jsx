c243c010caad6b9d91524dad20d26af024da8a7b
gjm1026 <gjm1026@naver.com>
2020-08-19 18:32:42 +0900

    yarn add react-google-button, OAUTH 구현방법 수정

--- src/signin/components/SignIn.jsx
+++ src/signin/components/SignIn.jsx
@@ -4,6 +4,7 @@ import GitHubLogin from 'react-github-login';
 import { Grid } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import GithubButton from 'react-github-login-button';
+import GoogleButton from 'react-google-button';
 import axios from 'axios';
 import './item.css';
 
@@ -15,22 +16,22 @@ function SignIn(props) {
   const redirect_uri = 'http://localhost:3000/signup';
   const githubPostUri = 'http://github.com/login/oauth/access_token';
 
-  const handleSuccessGoogle = (res) => {
-    console.log('[로그인 성공] 현재 사용자 : ', res);
-    const googleToken = res.wc.access_token;
-    // 서버url({
-    //   method: 'post',
-    //   url: '/signin/google',
-    //   data: googleToken,
-    // })
-    //   .then((res) => {
-    //     console.log('토큰 전송 성공', res.data);
-    //   })
-    //   .catch((err) => {
-    //     console.log('토큰 전송 실패', err);
-    //   });
-    props.history.replace('/signup');
-  };
+  // const handleSuccessGoogle = (res) => {
+  //   console.log('[로그인 성공] 현재 사용자 : ', res);
+  //   const googleToken = res.wc.access_token;
+  //   // 서버url({
+  //   //   method: 'post',
+  //   //   url: '/signin/google',
+  //   //   data: googleToken,
+  //   // })
+  //   //   .then((res) => {
+  //   //     console.log('토큰 전송 성공', res.data);
+  //   //   })
+  //   //   .catch((err) => {
+  //   //     console.log('토큰 전송 실패', err);
+  //   //   });
+  //   props.history.replace('/signup');
+  // };
 
   const handleSuccessGitHub = (res) => {
     /* 깃헙에서 accesstoken 받아와서 서버 /signin/github으로 전송 
@@ -57,18 +58,23 @@ function SignIn(props) {
     console.log('[로그인 실패] ', err);
   };
 
+  const handleClick = (res) => {
+    window.location.replace('http://localhost:8080/oauth2/authorize/google');
+  };
+
   return (
     <div>
       <h2 style={{ marginTop: 60, marginBottom: 50 }}>Sign In</h2>
       <div className="line" />
 
-      <GoogleLogin
+      <GoogleButton className="googlelogin" onClick={handleClick} />
+      {/* <GoogleLogin
         clientId={googleClientId}
         buttonText="Sign In with Google"
         onSuccess={handleSuccessGoogle}
         onFailure={handleFailure}
         isSignedIn={true}
-      />
+      /> */}
       <div className="space1" />
       <Grid>
         <GitHubLogin
