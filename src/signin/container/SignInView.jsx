2ae286688348ef63446b61429957d794612105b8
gjm1026 <gjm1026@naver.com>
2020-08-19 19:24:03 +0900

    SignInView 주석 추가

--- src/signin/container/SignInView.jsx
+++ src/signin/container/SignInView.jsx
@@ -11,6 +11,8 @@ function SignInView(props) {
         <Paper variant="elevation" elevation={10} className="item-align">
           <Grid>
             <br/>
+            {/* TODO : css 속성으로 여백 줘도 안먹히네요 Sign In 글자 위에 여백이 안생겨서 
+                여백 설정할 수 있는 방법이 지금은 <br> 뿐인 것 같아서 일단 이걸루 해놨구여 해결방법이 생기면 수정할 예정입니다 */}
             <SignIn />
           </Grid>
         </Paper>
